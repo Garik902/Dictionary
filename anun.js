de25ee2e27726809617629fa1b945c72d297bdf1
Garik902 <83010376+Garik902@users.noreply.github.com>
2021-04-22 14:50:18 +0400

    Add files via upload

---
+++ anun.js
@@ -0,0 +1,116 @@
+                                            function poxel1()
+			{
+							var myPar = document.getElementById("1");
+							myPar.innerHTML="խոնավ";
+							}
+                                            function poxel2()
+			{
+							var myPar1 = document.getElementById("2");
+							myPar1.innerHTML="ուրախանալ";
+							}
+							               function poxel3()
+			{
+							var myPar2 = document.getElementById("3");
+							myPar2.innerHTML="փախչել";
+							}
+											function poxel4()
+			{
+							var myPar3 = document.getElementById("4");
+							myPar3.innerHTML="աստվածային";
+							}
+							                function poxel5()
+			{
+							var myPar4 = document.getElementById("5");
+							myPar4.innerHTML="նվիրվածություն";
+							}
+                                            function poxel6()
+			{
+							var myPar5 = document.getElementById("6");
+							myPar5.innerHTML="խնջույք";
+							}
+							               function poxel7()
+			{
+							var myPar6 = document.getElementById("7");
+							myPar6.innerHTML="կատակ";
+							}
+            
+											function poxel8()
+			{
+							var myPar7 = document.getElementById("8");
+							myPar7.innerHTML="պարտականություն";
+							}
+							   function poxel9()
+			{
+							var myPar8 = document.getElementById("9");
+							myPar8.innerHTML="արատ";
+							}
+
+											function poxel10()
+			{
+							var myPar9 = document.getElementById("10");
+							myPar9.innerHTML="թմրություն";
+							}
+
+											function poxel11()
+			{
+							var myPar10 = document.getElementById("11");
+							myPar10.innerHTML="լարվածություն";
+                            }
+                            					
+								            function poxel12()
+			{
+							var myPar11 = document.getElementById("12");
+							myPar11.innerHTML="ցատկել";
+							}
+
+											function poxel13()
+			{
+							var myPar12 = document.getElementById("13");
+							myPar12.innerHTML="սեղմել";
+							}
+
+											function poxel14()
+			{
+							var myPar13 = document.getElementById("14");
+							myPar13.innerHTML="շրջապատել";
+							}
+
+											function poxel15()
+			{
+							var myPar14 = document.getElementById("15");
+							myPar14.innerHTML="դրդել";
+							}
+							                function poxel16()
+			{
+							var myPar15 = document.getElementById("16");
+							myPar15.innerHTML="հուսալքել";
+							}
+		                                    function poxel17()
+		{
+							var myPar16 = document.getElementById("17");
+							myPar16.innerHTML="անհանգստանալ";
+							}
+									        function poxel18()
+		{
+							var myPar17 = document.getElementById("18");
+							myPar17.innerHTML="արհեստական";
+							}
+							               function poxel19()
+		{
+							var myPar18 = document.getElementById("19");
+							myPar18.innerHTML="թուլացնել";
+							}
+							               function poxel20()
+		{
+							var myPar19 = document.getElementById("20");
+							myPar19.innerHTML="սպանել";
+							}
+		
+		
+		
+		
+
+
+
+
+
